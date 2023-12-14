8b2cbc1f30ecd315c3d65dd6639814631f1654c4
leroy <leroy.kariuki@student.moringaschool.com>
2023-12-14 08:49:33 +0300

    cli.py

--- lib/cli.py
+++ lib/cli.py
@@ -15,3 +15,32 @@ while True:
     @click.group()
     def cli():
         "The Museum"
+
+    @cli.command()
+    def open():
+        # stack to store history
+        history = []
+        def append(func):
+            history.append(func)
+
+        def back():
+            function = history.pop()
+            function()
+
+        """Enter The Museum"""
+        museum_dict = {}
+        
+        # Display available museums
+        def display_museums():
+            invite = "Available Museums: "
+            click.echo(invite.center(100))
+            list_museums = session.query(Museums).all()
+            session.close()
+            for i, museum in enumerate(list_museums, 1):
+                click.echo(f"{i}. {museum.name}")
+                museum_dict[i] = museum.name
+            append(display_museums)
+        display_museums()
+    
+
+        museum_choice = click.prompt("\nSelect Museum", type=int)
