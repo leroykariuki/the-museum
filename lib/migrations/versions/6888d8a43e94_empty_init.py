15f4c9996ff39adf0197aaa4d25b3cc1e91a3c52
leroy <leroy.kariuki@student.moringaschool.com>
2023-12-14 09:48:59 +0300

    migrations

---
+++ lib/migrations/versions/6888d8a43e94_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 6888d8a43e94
+Revises: 
+Create Date: 2023-12-14 09:43:00.889797
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '6888d8a43e94'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
